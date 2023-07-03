98c6172e838c9e2a6d286e9ed9bca1e3ab75c58e
Raj Chavda <raj.theappideas@gmail.com>
2023-07-03 18:57:49 +0530

    miner change

--- src/components/MyAccount/OrderHIstory.js
+++ src/components/MyAccount/OrderHIstory.js
@@ -52,41 +52,44 @@ const OrderHIstory = () => {
               </thead>
               <tbody className="bg-white text-BLACK text-base">
                 {completedOrders.length > 0 && !loading ? (
-                  completedOrders.slice().reverse().map((order) => (
-                    <tr className="text-center" key={order?._id}>
-                      <td
-                        onClick={() => {
-                          setShowSingleOrder(true);
-                          setOrderId(order?._id);
-                        }}
-                        className="px-3 py-5 underline text-PRIMARY font-semibold cursor-pointer"
-                      >
-                        {order?.orderId}
-                      </td>
-                      {/* <td className="px-3 py-5 whitespace-nowrap">
+                  completedOrders
+                    .slice()
+                    .reverse()
+                    .map((order) => (
+                      <tr className="text-center" key={order?._id}>
+                        <td
+                          onClick={() => {
+                            setShowSingleOrder(true);
+                            setOrderId(order?._id);
+                          }}
+                          className="px-3 py-5 underline text-PRIMARY font-semibold cursor-pointer"
+                        >
+                          {order?.orderId}
+                        </td>
+                        {/* <td className="px-3 py-5 whitespace-nowrap">
                   {order?.items?.map((item) => item?.type)}
                 </td> */}
-                      <td className="px-3 py-5 whitespace-nowrap">
-                        {moment(order?.orderDate).format("LL")}
-                      </td>
-                      <td className="px-3 py-5 whitespace-nowrap">
-                        {order?.items?.length}
-                      </td>
-                      <td className="px-3 py-5 whitespace-nowrap">
-                        {/* {order?.items?.reduce((acc, curr) => {
+                        <td className="px-3 py-5 whitespace-nowrap">
+                          {moment(order?.orderDate).format("LL")}
+                        </td>
+                        <td className="px-3 py-5 whitespace-nowrap">
+                          {order?.items?.length}
+                        </td>
+                        <td className="px-3 py-5 whitespace-nowrap">
+                          {/* {order?.items?.reduce((acc, curr) => {
                     return acc + curr?.quantity;
                   }, 0)} */}
-                        {order?.totalQuantity}
-                      </td>
-                      <td className="px-3 py-5 whitespace-nowrap">
-                        ${order?.total}
-                      </td>
-                      <td className="px-3 py-5 whitespace-nowrap">$0</td>
-                      <td className="px-3 py-5 whitespace-nowrap">
-                        ${order?.total}
-                      </td>
-                    </tr>
-                  ))
+                          {order?.totalQuantity}
+                        </td>
+                        <td className="px-3 py-5 whitespace-nowrap">
+                          ${parseFloat(order?.total).toFixed(2)}
+                        </td>
+                        <td className="px-3 py-5 whitespace-nowrap">$0</td>
+                        <td className="px-3 py-5 whitespace-nowrap">
+                          ${parseFloat(order?.total).toFixed(2)}
+                        </td>
+                      </tr>
+                    ))
                 ) : (
                   <>
                     <tr>
@@ -118,77 +121,80 @@ const OrderHIstory = () => {
           <table className="w-full table-auto overflow-hidden md:hidden">
             <tbody className="bg-white text-BLACK text-base">
               {completedOrders.length > 0 && !loading ? (
-                completedOrders.slice().reverse().map((order) => (
-                  <tr className="flex flex-col w-full" key={order?._id}>
-                    <tr className="text-center w-full flex flex-row">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Order no")}.
-                      </th>
-                      <td
-                        onClick={() => {
-                          setShowSingleOrder(true);
-                          setOrderId(order?._id);
-                        }}
-                        className="p-2 underline w-full text-PRIMARY font-semibold cursor-pointer"
-                      >
-                        {order?.orderId}
-                      </td>
-                    </tr>
-                    {/* <td className="p-2 whitespace-nowrap">
+                completedOrders
+                  .slice()
+                  .reverse()
+                  .map((order) => (
+                    <tr className="flex flex-col w-full" key={order?._id}>
+                      <tr className="text-center w-full flex flex-row">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Order no")}.
+                        </th>
+                        <td
+                          onClick={() => {
+                            setShowSingleOrder(true);
+                            setOrderId(order?._id);
+                          }}
+                          className="p-2 underline w-full text-PRIMARY font-semibold cursor-pointer"
+                        >
+                          {order?.orderId}
+                        </td>
+                      </tr>
+                      {/* <td className="p-2 whitespace-nowrap">
                   {order?.items?.map((item) => item?.type)}
                 </td> */}
-                    <tr className="w-full">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Order date")}
-                      </th>{" "}
-                      <td className="p-2 text-center w-full">
-                        {moment(order?.orderDate).format("LL")}
-                      </td>
-                    </tr>
-                    <tr className="w-full">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Items")}
-                      </th>
-                      <td className="p-2 text-center w-full">
-                        {order?.items?.length}
-                      </td>
-                    </tr>
-                    <tr className="w-full">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Quantity")}
-                      </th>
-                      <td className="p-2 text-center w-full">
-                        {/* {order?.items?.reduce((acc, curr) => {
+                      <tr className="w-full">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Order date")}
+                        </th>{" "}
+                        <td className="p-2 text-center w-full">
+                          {moment(order?.orderDate).format("LL")}
+                        </td>
+                      </tr>
+                      <tr className="w-full">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Items")}
+                        </th>
+                        <td className="p-2 text-center w-full">
+                          {order?.items?.length}
+                        </td>
+                      </tr>
+                      <tr className="w-full">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Quantity")}
+                        </th>
+                        <td className="p-2 text-center w-full">
+                          {/* {order?.items?.reduce((acc, curr) => {
                       return acc + curr?.quantity;
                     }, 0)} */}
-                        {order?.totalQuantity}
-                      </td>
+                          {order?.totalQuantity}
+                        </td>
+                      </tr>
+                      <tr className="w-full">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Amount")}
+                        </th>
+                        <td className="p-2 text-center w-full">
+                          ${parseFloat(order?.total).toFixed(2)}
+                        </td>
+                      </tr>
+                      <tr className="w-full">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Tax")}
+                        </th>
+                        <td className="p-2 text-center w-full">$0</td>
+                      </tr>
+                      <tr className="w-full">
+                        <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
+                          {t("Total")}
+                        </th>
+                        <td className="p-2 text-center w-full">
+                          ${parseFloat(order?.total).toFixed(2)}
+                        </td>
+                      </tr>
+                      <hr className="my-1 " />
                     </tr>
-                    <tr className="w-full">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Amount")}
-                      </th>
-                      <td className="p-2 text-center w-full">
-                        ${order?.total}
-                      </td>
-                    </tr>
-                    <tr className="w-full">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Tax")}
-                      </th>
-                      <td className="p-2 text-center w-full">$0</td>
-                    </tr>
-                    <tr className="w-full">
-                      <th className="bg-PRIMARY text-white p-2 text-center min-w-[5rem] max-w-[5rem]">
-                        {t("Total")}
-                      </th>
-                      <td className="p-2 text-center w-full">
-                        ${order?.total}
-                      </td>
-                    </tr>
-                    <hr className="my-1 " />
-                  </tr>
-                ))
+                  ))
               ) : (
                 <>
                   <tr>
