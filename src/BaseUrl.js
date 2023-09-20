1c92ee10446161211d3b95e395972b3e18be2f1b
Raj Chavda <raj.theappideas@gmail.com>
2023-09-20 15:10:35 +0530

    miner change

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
-// export default axios.defaults.baseURL = "https://admin.familymaidllc.com";
 export default axios.defaults.baseURL = "https://admin.dollarempirellc.com";
-// export default axios.defaults.baseURL = "https://dollarempire.onrender.com";
 
 const token = JSON.parse(window.localStorage.getItem("token"));
 
 export const PostUrl = axios.create({
-  // baseURL: "https://dollarempire.onrender.com/api",
   baseURL: "https://admin.dollarempirellc.com/api",
-  // baseURL: "https://admin.familymaidllc.com/api",
   method: "post",
   headers: {
     "Accept-Language": JSON.parse(window.localStorage.getItem("user_lang")),
@@ -19,7 +15,6 @@ export const PostUrl = axios.create({
 
 export const GetUrl = axios.create({
   baseURL: "https://admin.dollarempirellc.com/api",
-  // baseURL: "https://dollarempire.onrender.com/api",
   method: "get",
   headers: {
     "Accept-Language": JSON.parse(window.localStorage.getItem("user_lang")),
